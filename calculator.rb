dddb33d5235d73f4b17348e87ffdf2914c437b85
Ian Wright <wright.ianb@gmail.com>
2017-01-25 15:30:04 -0700

    added method refactoring, clear function, and recursive interface

--- calculator.rb
+++ calculator.rb
@@ -1,45 +1,47 @@
-puts 'Welcome to the Ruby Calculator'
-
 @operators = ['+', '-', '*', '/']
 
+def calculator_interface
+  puts 'Welcome to the Ruby Calculator'
+
+  puts "What is the first Number?"
+  first_num = valid_number_filter(gets.chomp)
+
+  puts "What is the operator?"
+  operator = gets.strip
+  unless @operators.include?(operator)
+    puts "Incorrect Operator: Only + - / * are supported."
+    exit(0)
+  end
+
+  puts "What is the second Number?"
+  second_num = valid_number_filter(gets.chomp)
+
+  puts operate(first_num, operator, second_num)
+
+  calculator_interface
+end
+
+def operate(num1, operator, num2)
+  case operator
+    when "+"
+      return num1 + num2
+    when "-"
+      return num1 - num2
+    when "*"
+      return num1 * num2
+    when "/"
+      return num1 / num2
+  end
+end
+
 def valid_number_filter(input)
+  calculator_interface() if input == "clear"
   if input == "0" || input.to_i != 0 
     return input.to_i
   else
     puts "Not A Number"
-    exit(0)
+    calculator_interface
   end
 end
 
-# I need to get the left hand for the equation
-
-puts "What is the first Number?"
-first_num = valid_number_filter(gets.chomp)
-# first_num = gets.strip
-# input_error("Not a number") unless valid_number_input?(first_num)
-# first_num = first_num.to_i
-
-# I need to get the operator for the equation
-puts "What is the operator?"
-operator = gets.strip
-unless @operators.include?(operator)
-  puts "Incorrect Operator"
-  exit(0)
-end
-
-# I need to get the right hand operator for the equation
-puts "What is the first Number?"
-second_num = valid_number_filter(gets.chomp)
-
-# I need to calculate
-case operator
-  when "+"
-    puts first_num + second_num
-  when "-"
-    puts first_num - second_num
-  when "*"
-    puts first_num * second_num
-  when "/"
-    puts first_num / second_num
-end
-
+calculator_interface
