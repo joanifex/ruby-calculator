59d1402e38e3e91c661eea4687e6485ae83fbacc
Ian Wright <wright.ianb@gmail.com>
2017-01-25 12:13:35 -0700

    pseudocode calculator

--- calculator.rb
+++ calculator.rb
@@ -1,1 +1,7 @@
 puts 'Welcome to the Ruby Calculator'
+
+# I need to get the left hand for the equation
+# I need to get the operator for the equation
+# I need to get the right hand operator for the equation
+# I need to calculate
+# I need to show the result
