2a5aed7bbc1e8b15ea001067af856ee9d51667f5
Ian Wright <wright.ianb@gmail.com>
2017-01-25 15:05:28 -0700

    add valid number filter

--- calculator.rb
+++ calculator.rb
@@ -1,7 +1,54 @@
 puts 'Welcome to the Ruby Calculator'
 
+@operators = ['+', '-', '*', '/']
+
+def valid_number_input?(input)
+  input == "0" || input.to_i != 0 ? true : false
+end
+
+def valid_number_filter(input)
+  if input == "0" || input.to_i != 0 
+    return input.to_i
+  else
+    puts "Not A Number"
+    exit(0)
+  end
+end
+
+def input_error(error)
+  puts error
+  exit(0)
+end
+
 # I need to get the left hand for the equation
+
+puts "What is the first Number?"
+first_num = valid_number_filter(gets.chomp)
+# first_num = gets.strip
+# input_error("Not a number") unless valid_number_input?(first_num)
+# first_num = first_num.to_i
+
 # I need to get the operator for the equation
+puts "What is the operator?"
+operator = gets.strip
+unless @operators.include?(operator)
+  puts "Incorrect Operator"
+  exit(0)
+end
+
 # I need to get the right hand operator for the equation
+puts "What is the first Number?"
+second_num = gets.strip.to_i
+
 # I need to calculate
-# I need to show the result
+case operator
+  when "+"
+    puts first_num + second_num
+  when "-"
+    puts first_num - second_num
+  when "*"
+    puts first_num * second_num
+  when "/"
+    puts first_num / second_num
+end
+
